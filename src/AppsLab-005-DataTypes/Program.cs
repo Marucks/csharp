5319e14418f6fe98a11e616655a6be3d59e143f5
arendarikm25 <arendarikm25@Z00T1-06>
2025-09-10 13:08:48 +0200

    Premenné

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -6,7 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int myAge = 15;
+            /*int myAge = 15;
             Console.WriteLine(myAge);
 
             float pi = 3.14f;
@@ -20,11 +20,68 @@ namespace MyApp
 
             bool isAdult = false;
             Console.WriteLine(isAdult);
+            */
+
+            /*
+            int a = 10;
+            int b = 0;
+
+            int sum = a + b;
+            Console.WriteLine(sum);
+            // scitavanie A a B
+
+            Console.WriteLine(a - b);
+            //odtivatanie A a B
+
+            Console.WriteLine(a * b);
+            // nasobenie
+
+            Console.WriteLine(a + b);
+            // delenie
+
+            Console.WriteLine("Diki brasko");
+            */
+
+            /*
+            int birthDay = 16;
+            int birthMonth = 6;
+            int birthYear = 2010;
+
+            int birthSum = birthDay + birthMonth + birthYear;
+            Console.WriteLine(birthSum);
+            Console.WriteLine(birthSum * 10);
+
+            Console.WriteLine((birthDay + birthMonth) * birthYear);
+
+
+            Console.WriteLine(5F / 3F);
+
+            string meno = "Example";
+            Console.WriteLine(meno + birthYear);
+
+            Console.WriteLine(meno + (birthDay + birthMonth));
+            */
+
+
+            bool result = 6 > 3;
+            Console.WriteLine(result);
+
+            string marek = "Marek;";
+            string michal = "Michal";
+            Console.WriteLine(marek == michal);
+            
+            
+
+
+
+
+
+
 
             /*
              * switch between git changes
              * else solution explorer
-             * */
+             */
 
 
         }
