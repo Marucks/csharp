2bbf98dfe5cd76923ba89fa1ad1f657329230adc
arendarikm25 <arendarikm25@Z00T1-06>
2025-10-01 12:59:19 +0200

    Polia

--- Cvicenie_polia/Program.cs
+++ Cvicenie_polia/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Cvicenie_polia
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cvicenie_polia
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*int[] numbers = new int[7];
+
+            numbers[0] = 10;
+            numbers[1] = 15;
+            numbers[2] = 35;
+            numbers[3] = 48;
+            numbers[4] = 2;
+            numbers[5] = 1;
+            numbers[6] = 19;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[4]);
+            Console.WriteLine(numbers[5]);
+            Console.WriteLine(numbers[6]);
+            Console.WriteLine(numbers[7]);
+            Console.WriteLine(numbers[8]);*/
+
+            /* int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine(sum);*/
+
+
+            /*int sum = 0;
+            foreach (var item in numbers) ;
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);*/
+            
+            
+
+
+
+
         }
     }
 }
