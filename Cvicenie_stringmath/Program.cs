681e65429adb0ebde8eeee6a4d6a15faccbaf123
arendarikm25 <arendarikm25@Z00T1-06>
2025-10-15 12:08:58 +0200

    idkkk

---
+++ Cvicenie_stringmath/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+
+namespace Cvicenie_stringmath
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] text = File.ReadAllLines("People_100.csv");
+            Kamnem(text);
+        }
+
+        public static void Kamnem(string[] text)
+        {
+            int sum = 0;
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(';');
+                int RodneCislo = int.Parse(splits[2]);
+                sum += RodneCislo;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Cvicenie_subor/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Cvicenie_subor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines("People_100.csv");
+            WriteRodneCislo(text);
+            MoneyCountAverage(text);
+            MinMoneyCount(text);
+        }
+
+        public static void MoneyCountAverage(string[] text)
+        {
+
+        }
+        public static void WriteRodneCislo(string[] text)
+        {
+
+        }
+        public static void MinMoneyCount(string[] text)
+        {
+            int MinValue = 99999999;
+            string minValuePerson = "";
+            foreach (string line in text.Skip(1))
+            {
+                string[] split = line.Split(';');
+                int accountValue = int.Parse(split[4]);
+                if (accountValue < MinValue)
+                {
+                    MinValue = accountValue;
+                    minValuePerson = split[0] + " " + split[1];
+                }
+            }
+            Console.WriteLine(minValuePerson);
+        }
+
+    }
+}
